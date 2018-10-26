11e5ebe98c1d4783f6304dee926e27a0b92ea8fe
Kim Garrard <kimgarrard030@gmail.com>
2018-10-26 11:42:59 +0200

    Update

--- js/transavia.js
+++ js/transavia.js
@@ -14,3 +14,29 @@ for (var i = 0; i < titel.length; i++) {
         parentElement.classList.toggle("active");
     });
 }
+
+
+var knop = document.getElementById('knop');
+
+function laden(value) {
+    document.getElementById('replace').classList.add('replace');
+    document.getElementById('test1').classList.remove('hiddenload1');
+    document.getElementById('test2').classList.remove('hiddenload2');
+    document.getElementById('test3').classList.remove('hiddenload3');
+}
+
+function stop(value) {
+    document.getElementById('test3').classList.add('hiddenload3');
+}
+
+
+knop.addEventListener('click', laden);
+document.getElementById('test3').addEventListener('transitionend', stop);
+
+
+
+function loadaway(value) {
+    document.getElementById('test3').classList.add('hidden');
+}
+
+var myTimer = setInterval(loadaway, 2000);
